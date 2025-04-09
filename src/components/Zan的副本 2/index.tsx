fb1964ecdc378ecfe0e64e5392b8871900b82558
ggchinazhangwei <714349513@qq.com>
2025-04-09 14:01:08 +0800

    fix

---
+++ src/components/Zan的副本 2/index.tsx
@@ -0,0 +1,39 @@
+import React, { memo } from "react";
+import { Button, Popover } from "antd";
+import styles from "./index.less";
+
+interface IProps {
+  text: any;
+}
+
+///这组件写的有问题  popover会重定位
+const content = (
+  <div className={styles.imgWrap}>
+    <img
+      src={`http://h5.dooring.cn/uploads/WechatIMG2_17969ccfe40.jpeg`}
+      alt="sponsorship"
+    />
+  </div>
+);
+
+export default memo(function ZanPao(props: IProps) {
+  const {
+    text = (
+      <Button
+        type="primary"
+        danger
+        style={{ background: "red !important" }}
+        size="large"
+      >
+        支持开源, 请作者喝茶~
+      </Button>
+    )
+  } = props;
+  return (
+    <div className={styles.takeCat}>
+      <Popover placement="top" title={null} content={content} trigger="hover">
+        {text}
+      </Popover>
+    </div>
+  );
+});
